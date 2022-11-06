bb55afcb1a972c461dcf04747acba5613340bfb9
BL4IS3 <bl4is3@free.fr>
2022-11-06 19:49:04 +0100

    mise en place menu et main pour premier lancement du logiciel

---
+++ main.py
@@ -0,0 +1,11 @@
+from models.tournament import Tournament
+from views.view import View
+
+
+def main():
+    debut = View()
+    debut.prompt_principal_menu()
+
+
+if __name__ == "__main__":
+    main()
