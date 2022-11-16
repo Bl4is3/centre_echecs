1fa9d79a8e70e73f797629ac702af88141625a3c
BL4IS3 <bl4is3@free.fr>
2022-11-16 20:12:05 +0100

    Ajout des fonctionnalités Round, load-tournament

--- controllers/base.py
+++ controllers/base.py
@@ -2,18 +2,23 @@
 
 from models.player import Player
 from models.tournament import Tournament
-from tinydb import TinyDB, Query
-from pathlib import Path
+from models.round import Round
+from models.database import Database
+from views.view import View
+from tinydb import Query, where
+
+from datetime import datetime
 
 
 class Controller:
     """Main controller."""
 
-    def __init__(self, view):
+    def __init__(self):
         """Has a a view."""
         # models
-        self.view = view
-
+        self.view = View()
+        self.db = Database('db.json')
+    
     def create_player(self, detail):
         """ Instance a new player"""
         first_name = detail[0]
@@ -21,29 +26,15 @@ class Controller:
         sex = detail[2]
         date_birthday = detail[3]
         rank = detail[4]
-        id_player = self.get_last_id('players') + 1
-        player = Player(id_player, first_name, last_name, sex,
+        id = self.db.get_last_id('players') + 1
+        player = Player(id, first_name, last_name, sex,
                         date_birthday, rank)
-        serialized_player = self.serialized_player(player)
-        self.add_player_to_db(serialized_player)
-
-    @staticmethod
-    def serialized_player(player):
-        """Serialize a player"""
-        serialized_player = {
-            'id': player.id,
-            'first_name': player.first_name,
-            'last_name': player.last_name,
-            'sex': player.sex,
-            'date_birthday': player.date_birthday,
-            'rank': player.rank
-        }
-        return serialized_player
-
-    @staticmethod
-    def unserialized_player(serialized_player):
+        serialized_player = player.serialized_player(player)
+        self.db.add_player_to_db(serialized_player)
+    
+    def unserialized_player(self, serialized_player):
         """Unserialize a player"""
-        id_player = serialized_player['id']
+        id = serialized_player['id']
         first_name = serialized_player['first_name']
         last_name = serialized_player['last_name']
         sex = serialized_player['sex']
@@ -51,118 +42,121 @@ class Controller:
         rank = serialized_player['rank']
 
         return Player(
-            id_player,
+            id,
             first_name,
             last_name,
             sex,
             date_birthday,
             rank)
 
-    @staticmethod
-    def serialized_tournament(tournament):
-        """Serialize a tournament"""
-        serialized_tournament = {
-            'id': tournament.id,
-            'name': tournament.name,
-            'place': tournament.place,
-            'date_beginning': tournament.date_beginning,
-            'players_tournament': tournament.players,
-            'timer': tournament.timer,
-            'description': tournament.description,
-            'finished': tournament.finished,
-            'number_of_rounds': tournament.number_of_rounds
-        }
-        return serialized_tournament
+    def create_tournament(self, detail):
+        name = detail[0]
+        place = detail[1]
+        dates = detail[2]
+        players = detail[3]
+        timer = detail[4]
+        description = detail[5]
+        number_of_rounds = detail[6]
+        scores = [0,0,0,0,0,0,0,0]
+        rounds_ok = 0
+        id = self.db.get_last_id('tournaments') + 1
+        tournament = Tournament(
+            id,
+            name,
+            place,
+            dates,
+            players,
+            scores,
+            timer,
+            description,
+            number_of_rounds, 
+            rounds_ok
+            )
+        serialized_tournament = tournament.serialized_tournament(tournament)
+        self.db.add_tournament_to_db(serialized_tournament)
 
-    @staticmethod
-    def unserialized_tournament(serialized_tournament):
-        """Unserialize a tournament"""
+        return tournament
+
+    def unserialized_tournament(self, serialized_tournament):
+        """ Instance a new player"""
         id = serialized_tournament['id']
         name = serialized_tournament['name']
         place = serialized_tournament['place']
-        date_beginning = serialized_tournament['date_beginning']
-        players = serialized_tournament['players_tournament']
+        dates = serialized_tournament['dates']
+        players = serialized_tournament['players']
+        scores = serialized_tournament['scores']
         timer = serialized_tournament['timer']
         description = serialized_tournament['description']
-        finished = serialized_tournament['finished']
         number_of_rounds = serialized_tournament['number_of_rounds']
+        rounds_ok = serialized_tournament['rounds_ok']
+        finished = serialized_tournament['finished']
 
         return Tournament(
             id,
             name,
             place,
-            date_beginning,
+            dates,
             players,
+            scores,
             timer,
             description,
-            finished,
-            number_of_rounds)
+            number_of_rounds,
+            rounds_ok,
+            finished)
+    
+    def create_first_round(self, tournament_id):
+        """Create first round"""
+        name = 'Round_1'
+        datetime_beginning = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+        id = self.db.get_last_id('rounds') + 1
+        round = Round(id, 1, tournament_id, datetime_beginning)
+        serialized_round = round.serialized_round(round)
+        self.db.add_round_to_db(serialized_round)
 
-    @staticmethod
-    def initialize_database():
-        """Create database  and tables (if not exist)"""
-        filename = r'db.json'
-        fileobj = Path(filename)
-        if fileobj.is_file():
-            return
-        else:
-            db = TinyDB('db.json')
-            players_table = db.table('players')
-            tournaments_table = db.table('tournaments')
-            players_table.truncate()
-            tournaments_table.truncate()
+        return round
 
-    def get_players_from_db(self):
-        """ Get players from db"""
-        db = TinyDB('db.json')
-        players_table = db.table('players')
-        serialized_players = players_table.all()
-        return serialized_players
+    def create_other_round(self, round_number, tournament_id):
+        """Create other round"""
+        name = 'Round_' + round_number
+        datetime_beginning = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
+        id = self.db.get_last_id('rounds') + 1
+        round = Round(id, round_number, tournament_id, datetime_beginning)
+        serialized_round = round.serialized_round(round)
+        self.db.add_round_to_db(serialized_round)
 
-    @staticmethod
-    def get_tournaments_from_db():
-        """ Get tournaments from db"""
-        db = TinyDB('db.json')
-        tournaments_table = db.table('tournaments')
-        serialized_tournaments = tournaments_table.all()
-        return serialized_tournaments
+        return round
 
-    def add_player_to_db(self, serialized_player):
-        db = TinyDB('db.json')
-        players_table = db.table('players')
-        players_table.insert(serialized_player)
+    def unserialized_round(self, serialized_round):
+        """Unserialize a round"""
+        id = serialized_round['id']
+        number = serialized_round['number']
+        id_tournament = serialized_round['id_tournament']
+        datetime_beginning = serialized_round['datetime_beginning']
+        datetime_end = serialized_round['datetime_end']
 
-    def add_tournament_to_db(self, serialized_tournament):
-        db = TinyDB('db.json')
-        tournaments_table = db.table('tournaments')
-        tournaments_table.insert(serialized_tournament)
-
-    def create_tournament(self, detail):
-        name = detail[0]
-        place = detail[1]
-        date_beginning = detail[2]
-        players_tournament = detail[3]
-        timer = detail[4]
-        description = detail[5]
-        number_of_rounds = detail[6]
-        id_tournament = self.get_last_id('tournaments') + 1
-        tournament = Tournament(
+        return Round(
+            id,
+            number,
             id_tournament,
-            name,
-            place,
-            date_beginning,
-            players_tournament,
-            timer,
-            description,
-            number_of_rounds
-            )
-        serialized_tournament = self.serialized_tournament(tournament)
-        self.add_tournament_to_db(serialized_tournament)
+            datetime_beginning,
+            datetime_end)  
+    
+    def load_tournament(self, id_tournament):
+        tournaments = self.db.get_elements_from_db('tournaments')
+        for tournament in tournaments:
+            if tournament['id'] == int(id_tournament):
+                unserialized_tournament = self.unserialized_tournament(tournament)
+        if unserialized_tournament.rounds_ok == 0:
+            self.create_first_round(unserialized_tournament.id)
+        else:
+            self.create_other_round(unserialized_tournament.rounds_ok, )
+        unserialized_tournament.rounds_ok += 1
+        print("Round n°: ", unserialized_tournament.rounds_ok, " en cours: " )
+
 
     def get_list_tournaments_not_finished(self):
         """ Get tournaments in progress"""
-        db = TinyDB('db.json')
-        all_tournaments = db.table('tournaments')
+        all_tournaments = self.db.table('tournaments')
         list_tournaments = Query()
         tournaments_in_progress = all_tournaments.search(
             list_tournaments.finished == 'False')
@@ -172,82 +166,69 @@ class Controller:
             tournaments.append(unserialized_tournament)
         return tournaments
 
-    def get_last_id(self, element):
-        db = TinyDB('db.json')
-        name_table = str(element)
-        elements = db.table(name_table)
-        if elements:
-            el = elements.all()[-1]
-            last_id = el.doc_id
-        else:
-            last_id = 0
-
-        return last_id
-
-    def get_number_of_players(self):
-        db = TinyDB('db.json')
-        elements = db.table('players')
-        number = len(elements)
-        return number
-
     def sort_players_by_names(self, players):
         names = []
-        for play in players:
-            unserialized_player = self.unserialized_player(play)
-            names.append(unserialized_player.last_name)
+        for player in players:
+            names.append(player[0]['last_name'])
         sorted_names = sorted(names)
         sorted_player_by_names = []
         for name in sorted_names:
             for player in players:
-                unserialized_player = self.unserialized_player(player)
-                if name == unserialized_player.last_name:
-                    sorted_player_by_names.append(unserialized_player)
+                if name == player[0]['last_name']:
+                    sorted_player_by_names.append(player)
         return sorted_player_by_names
 
     def sort_players_by_rank(self, players):
         ranks = []
-        for play in players:
-            unserialized_player = self.unserialized_player(play)
+        for player in players:
+            unserialized_player = self.unserialized_player(player) 
             ranks.append(unserialized_player.rank)
         sorted_ranks = sorted(ranks)
         sorted_player_by_ranks = []
+        list_id = []
         for rank in sorted_ranks:
             for player in players:
                 unserialized_player = self.unserialized_player(player)
-                if rank == unserialized_player.rank:
-                    sorted_player_by_ranks.append(unserialized_player)
+                if rank == unserialized_player.rank:    
+                    if unserialized_player.id not in list_id:
+                        sorted_player_by_ranks.append(unserialized_player)
+                        list_id.append(unserialized_player.id)
         return sorted_player_by_ranks
 
     def run(self):
         """Run the game."""
         running = True
-        self.initialize_database()
-        self.get_players_from_db()
-        self.get_tournaments_from_db()
+        self.db.initialize_database()        
         choix = self.view.prompt_principal_menu()
         while running:
+            players = self.db.get_elements_from_db('players')
+            tournaments = self.db.get_elements_from_db('tournaments')
             if choix == "1":
                 choix = self.view.afficher_menu_tournoi()
             elif choix == "11":
-                number = self.get_number_of_players()
+                number = self.db.get_number_of_players()
                 if number < 8:
                     print("Attention, Il n'y a pas suffisamment de joueurs "
                           "inscrits pour débuter un tournoi")
                     choix = self.view.afficher_menu_joueur()
                 else:
                     elements_tournament = self.view.create_tournament()
-                    self.create_tournament(elements_tournament)
+                    tournament = self.create_tournament(elements_tournament) 
+                    self.create_first_round(tournament.id)
+                                     
                     choix = self.view.afficher_menu_tournoi()
             elif choix == "12":
-                choix = self.view.resume_tournament()
-            elif choix == "13":
-                tournois = self.get_tournaments_from_db()
-                tournaments = []
-                for tournoi in tournois:
-                    unserialized_tournament = self.unserialized_tournament(tournoi)
-                    tournaments.append(unserialized_tournament)
-
-                self.view.show_listing_all_tournaments(tournaments)
+                id_tournament = self.view.select_tournament()
+                self.load_tournament(id_tournament)
+            elif choix == "13":                
+                list_tournaments = []
+                for tournament in tournaments:
+                    unserialized_tournament = self.unserialized_tournament(tournament)
+                    list_tournaments.append(unserialized_tournament)
+                if not list_tournaments:
+                    print('\nIl n\'y a encore aucun tournoi d\'enregistré!' )
+                else:
+                    self.view.show_listing_all_tournaments(list_tournaments)
                 choix = self.view.afficher_menu_tournoi()
             elif choix == "16":
                 choix = self.view.prompt_principal_menu()
@@ -260,12 +241,14 @@ class Controller:
                 self.create_player(elements_player)
                 choix = self.view.afficher_menu_joueur()
             elif choix == "22":
-                players = self.get_players_from_db()
-                listing = self.sort_players_by_names(players)
+                list_players = []
+                for player in players:
+                   unserialized_player = self.unserialized_player(player) 
+                   list_players.append(unserialized_player)
+                listing = self.sort_players_by_names(list_players)
                 self.view.affichage_liste_joueurs_alphabetique(listing)
                 choix = self.view.afficher_menu_joueur()
             elif choix == "23":
-                players = self.get_players_from_db()
                 listing = self.sort_players_by_rank(players)
                 self.view.affichage_liste_joueurs_classement(listing)
                 choix = self.view.afficher_menu_joueur()
